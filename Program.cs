932edb3f450ef552de6842415b6af0bc927b4394
HABUMUGISH0784643608 <jeanclaudehabumugish81@gmail.com>
2018-12-04 16:08:00 +0200

    csharp work

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1;
+            double number2;
+            int choose;
+
+            Console.WriteLine("enter num1");
+            number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter number2");
+            number2 = Convert.ToDouble(Console.ReadLine());
+
+            var items = new Classarthmetic();
+            items.operators();
+
+            Console.WriteLine("choose an operation");
+            choose = Convert.ToInt32 (Console.ReadLine());
+
+            switch(choose)
+            {
+                case 1:
+                    Console.WriteLine("addition= {0}", items.addition(number1, number2));
+                    break;
+                case 2:
+                    Console.WriteLine("subtruction= {0}", items.subtruction(number1, number2));
+                    break;
+                case 3:
+                    Console.WriteLine("multiplication= {0}", items.multiplication(number1, number2));
+                    break;
+                case 4:
+                    Console.WriteLine("division= {0}", items.division(number1, number2));
+                    break;
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ classarithmetic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Classarithmetic
+    {
+         public double number1;
+        public double number2;
+        public double addition(double number1, double number2){
+            return (number1+number2);
+        }
+        public double subtruction(double number1, double number2){
+            return (number1-number2);
+        }
+        public double multiplication(double number1, double number2){
+            return (number1*number2);
+        }
+        public double division(double number1, double number2){
+            if (number2==0)
+            Console.WriteLine("it is impossible to divide any number by zero");
+            return (number1/number2); 
+        }
+
+        public void operators()
+        {
+            Console.WriteLine("addition");
+            Console.WriteLine("subtruction");
+            Console.WriteLine("multiplication");
+            Console.WriteLine("division");
+        }
+    }
+}
